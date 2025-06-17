b3ae44b266d83d92f472b07a47386fbf07443617
miokato <miokato@me.com>
2025-06-18 06:19:13 +0900

    Add ModelContainer.

--- PocketTown/PocketTownApp.swift
+++ PocketTown/PocketTownApp.swift
@@ -6,14 +6,29 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct PocketTownApp: App {
+    /// SwiftDataで保存した値をCloudKitで共有する
+    var modelContainer: ModelContainer = {
+        let schema = Schema([
+            MapPin.self,
+        ])
+        let config = ModelConfiguration(cloudKitDatabase: .automatic)
+        
+        do {
+            return try ModelContainer(for: schema, configurations: config)
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
     
     var body: some Scene {
         WindowGroup {
             MainView()
         }
-        .modelContainer(for: MapPin.self)
+        .modelContainer(modelContainer)
     }
 }
