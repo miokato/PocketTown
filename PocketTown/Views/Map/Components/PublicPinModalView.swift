879526bcda4236e67587cd889bcc60193606e488
miokato <miokato@me.com>
2025-06-22 06:57:26 +0900

    Update public pin modal.

--- PocketTown/Views/Map/Components/PublicPinModalView.swift
+++ PocketTown/Views/Map/Components/PublicPinModalView.swift
@@ -11,10 +11,45 @@ struct PublicPinModalView: View {
     let selectedPin: MapPin?
     
     var body: some View {
-        Text("PublicMapModalView")
+        Form {
+            if let pin = selectedPin {
+                Section {
+                    Text(pin.title)
+                    Text(pin.note)
+                }
+                Section {
+                    coordinateView(pin: pin)
+                } header: {
+                    Text("座標")
+                }
+            } else {
+                Text("存在しないピンです")
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func coordinateView(pin: MapPin) -> some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Image(systemName: "location")
+                Text("mapsheet.label.latitude") + Text(": \(pin.latitude, format: .number.precision(.fractionLength(6)))")
+                Text("mapsheet.label.longitude") + Text(": \(pin.longitude, format: .number.precision(.fractionLength(6)))")
+            }
+        }
+        .font(.caption)
+        .foregroundColor(.primary)
     }
 }
 
 #Preview {
-    PublicPinModalView(selectedPin: .makeSample(.init(latitude: 0, longitude: 0)))
+    PublicPinModalView(
+        selectedPin: MapPin(
+            title: "テストピン",
+            note: "テストノート",
+            latitude: 35.0,
+            longitude: 135.0,
+            isPublic: false
+        )
+    )
 }
